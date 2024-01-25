c97ee3a7bea0d0037df97f07e8be8a4218590960
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-01-26 00:00:05 +0200

    media mobile tablet for header, mobile menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const navButtonEl = document.querySelector('.nav-icon-btn');
+const navIconEl = document.querySelector('.nav-icon');
+const mobileMenuEl = document.querySelector('.header__top');
+
+navButtonEl.addEventListener('click', onNavButtonClick);
+
+function onNavButtonClick() {
+    navIconEl.classList.toggle('nav-icon--active');
+    mobileMenuEl.classList.toggle('header__top--mobile');
+    document.body.classList.toggle('no-scroll');
+}
