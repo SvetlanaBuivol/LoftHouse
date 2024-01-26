45e31f47978d18361e7ad827445c361afbfbbe2d
SvetlanaBuuvol <divinesvetok@gmail.com>
2024-01-26 22:23:54 +0200

    media tablet, phone mask js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ function onNavButtonClick() {
     mobileMenuEl.classList.toggle('header__top--mobile');
     document.body.classList.toggle('no-scroll');
 }
+
+// phone mask
+mask('[data-phone-input]');
+
+const phoneInputs = document.querySelectorAll('[data-phone-input]');
+phoneInputs.forEach((input)=>{
+	input.addEventListener('input', ()=>{
+		if (input.value == '+') input.value = '';
+	})
+	input.addEventListener('blur', ()=>{
+		if (input.value == '+') input.value = '';
+	})
+});
